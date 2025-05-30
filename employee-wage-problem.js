b1d1a73f9bc03cb7182e3694f466f1f860a6e89d
Ajitesh Madhu <113586996+Ajitesh00@users.noreply.github.com>
2025-05-30 11:04:54 +0530

    feat:Add Calculate Daily Employee Wage

--- employee-wage-problem.js
+++ employee-wage-problem.js
@@ -1,3 +1,6 @@
+const WAGE_PER_HOUR = 20;
+const FULL_DAY_HOUR = 8;
+
 // UC1: Check Employee Attendance
 function checkAttendance() {
     const status = Math.floor(Math.random() * 2); // 0 for absent, 1 for present
@@ -5,5 +8,17 @@ function checkAttendance() {
     return status;
 }
 
+// UC2: Calculate Daily Employee Wage
+function calculateDailyWage(empStatus) {
+    if (empStatus === 0) {
+        console.log("Employee is Absent. No wage calculated.");
+        return 0;
+    }
+    const wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+    console.log("Full-time Wages:", wage);
+    return wage;
+}
+
 console.log("Welcome to Employee Wage Computation Program on Master Branch");
-checkAttendance();
+const empStatus = checkAttendance();
+calculateDailyWage(empStatus);
